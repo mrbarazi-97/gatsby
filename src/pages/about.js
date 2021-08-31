0edd05b6d0af91156dbcddf07166d9bec5af8c2d
mrbarazi-97 <you@example.com>
2021-08-31 13:11:33 +0430

    second commit

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+
+
+const AboutUs=()=>{
+    return(
+        <main>
+            <title>About Us</title>
+            <h1>about us page</h1>
+            <p>a paragroh for my 'about us' page</p>
+        </main>
+    )
+}
+
+
+export default AboutUs
